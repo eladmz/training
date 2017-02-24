b509234d8cd7c34c34cf9ae70cd9790b645dd9a0
Elad Mizrahi <eladmz@gmail.com>
2017-02-24 13:16:37 +0200

    uploading tutorial 9

---
+++ CSharpTutorialBasics/CSharpTutorial9/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorial9
+{
+    abstract class Shape
+    {
+        public string Name { get; set; }
+
+        // You can define non-abstract
+        // methods in an abstract class
+        public virtual void GetInfo()
+        {
+            Console.WriteLine($"This is a {Name}");
+        }
+
+        // We want subclasses to override
+        // this method so mark it as abstract
+        // You can only make abstract methods 
+        // in abstract classes
+        public abstract double Area();
+    }
+}
