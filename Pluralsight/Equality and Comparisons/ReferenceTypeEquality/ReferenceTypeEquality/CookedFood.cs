fb69ce27d31de13b2e61f59819ce50e26864b4b3
Elad Mizrahi <elad.mizrahi1@il.ibm.com>
2017-03-07 14:52:48 +0200

    IComparable and IEquatable

---
+++ Pluralsight/Equality and Comparisons/ReferenceTypeEquality/ReferenceTypeEquality/CookedFood.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypeEquality
+{
+    public sealed class CookedFood : Food, IEquatable<CookedFood>
+    {
+        private string cookingMethod;
+
+        public string CookingMethod { get { return cookingMethod; } }
+
+        public CookedFood(string cookingMethod, string name, FoodGroup group) : base(name, group)
+        {
+            this.cookingMethod = cookingMethod;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", cookingMethod, Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!base.Equals(obj))
+                return false;
+            CookedFood rhs = (CookedFood)obj;
+            return this.cookingMethod == rhs.CookingMethod;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode() ^ this.cookingMethod.GetHashCode();
+        }
+
+        // this good only for sealed classes but it's better not to do it on reference types
+        public bool Equals(CookedFood other)
+        {
+            if (!base.Equals(other))
+                return false;
+            return this.cookingMethod == other.CookingMethod;
+        }
+
+        public static bool operator ==(CookedFood x, CookedFood y)
+        {
+            return object.Equals(x, y);
+        }
+
+        public static bool operator !=(CookedFood x, CookedFood y)
+        {
+            return !object.Equals(x, y);
+        }
+    }
+}
---
+++ Pluralsight/Equality and Comparisons/ReferenceTypeEquality/ReferenceTypeEquality/Food.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypeEquality
+{
+    public enum FoodGroup { Meat, Fruit, Vegetables, Sweets }
+
+    public class Food
+    {
+        private readonly string name;
+        private readonly FoodGroup group;
+
+        public string Name { get { return name; } }
+
+        public FoodGroup Group { get { return group; } }
+
+        public Food(string name, FoodGroup group)
+        {
+            this.name = name;
+            this.group = group;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
+
+        public static bool operator ==(Food x, Food y)
+        {
+            return object.Equals(x, y);
+        }
+
+        public static bool operator !=(Food x, Food y)
+        {
+            return !object.Equals(x, y);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (ReferenceEquals(obj, this))
+                return true;
+            if (obj.GetType() != this.GetType())
+                return false;
+            Food rhs = obj as Food;
+            return this.name == rhs.Name && this.group == rhs.Group;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.name.GetHashCode() ^ this.group.GetHashCode();
+        }
+
+    }
+}
