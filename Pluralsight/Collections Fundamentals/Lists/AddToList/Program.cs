bfc21bd56c99039674582bcc15e6c54feeff6b11
Elad Mizrahi <elad.mizrahi1@il.ibm.com>
2017-03-08 14:12:49 +0200

    some collections

---
+++ Pluralsight/Collections Fundamentals/Lists/AddToList/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddToList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var presidents = new List<string>(12){
+                "Jimmy Carter",
+                "Ronald Reagan",
+                "George HW Bush",
+                "Bill Clinton",
+                "George W Bush"
+            };
+
+            // adding an item to a list. 
+            presidents.Add("Barack Obama");
+
+            // two different ways of getting read-only access to the list - not really defend the list, still people can
+            // access/modify it using reflection or other "complex" ways.
+            var copy = new ReadOnlyCollection<string>(presidents);
+            // var copy = presidents.AsReadOnly();
+
+            BadCode(copy);
+            foreach (string president in presidents)
+                Console.WriteLine(president);
+        }
+
+
+        static void BadCode(IList<string> lst)
+        {
+            // this would throw an exception: lst is read-only
+            //			lst.RemoveAt(2);
+        }
+    }
+}
---
+++ Pluralsight/Collections Fundamentals/Lists/ObservableCollection/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObservableCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObservableCollection<string> presidents = new ObservableCollection<string>
+            {
+                "Jimmy Carter",
+                "Ronald Reagan",
+                "George HW Bush"
+            };
+            foreach (string president in presidents)
+                Console.WriteLine(president);
+            Console.WriteLine();
+
+            presidents.CollectionChanged += OnCollectionChanged;
+
+            presidents.Add("Bill Clinton");
+
+            foreach (string president in presidents)
+                Console.WriteLine(president);
+            Console.WriteLine();
+
+            presidents.Remove("Jimmy Carter");
+
+            foreach (string president in presidents)
+                Console.WriteLine(president);
+
+        }
+
+        // stop the debugger on calls to this and examine e to see what properties are available
+        // telling you what's changed in the collection
+        static void OnCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine("Change made! Action: {0} - added item {1} to list", e.Action, e.NewItems[0]);
+            }
+            else
+            {
+                Console.WriteLine("Change made! Action: {0} - removed item {1} from list", e.Action, e.OldItems[0]);
+            }
+            
+        }
+    }
+}
